f495cc5fac4543368b91c1b551afbef5cc88978e
PabloCampanini <pablocampanini00@gmail.com>
2024-05-21 00:35:19 -0300

    Ejercicio 1

--- Program.cs
+++ Program.cs
@@ -20,5 +20,31 @@ Console.WriteLine("valor de a:"+b);
 
 
 
-
 // *------ EJERCICIO 1 ------*
+/*
+
+Console.WriteLine("Ingrese un numero: ");
+string cadena = Console.ReadLine();
+
+int numero;
+
+if (int.TryParse(cadena, out numero))
+{
+    if (numero > 0)
+    {
+        char[] arreglo = cadena.ToCharArray();
+
+        Array.Reverse(arreglo);
+
+        Console.WriteLine($"Numero ingresado = {numero}\nNumero invertido = {new string(arreglo)}");
+    }
+}
+
+*/
+
+
+
+
+// *------ EJERCICIO 2 ------*
+
+
