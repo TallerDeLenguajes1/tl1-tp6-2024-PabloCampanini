bbfd460fc7ab2b720aef1d2c4b25bb92e749c7ea
PabloCampanini <pablocampanini00@gmail.com>
2024-05-27 02:49:28 -0300

    Ejercicio 4

--- Program.cs
+++ Program.cs
@@ -289,9 +289,228 @@ Console.WriteLine("\nIngrese una oracion que se concatenara con la primera:");
 
 string Oracion2 = Console.ReadLine();
 
-string concatenada = String.Concat(Oracion1," ", Oracion2);
+string Concatenada = String.Concat(Oracion1, " ", Oracion2);
 
 Console.WriteLine("\n\n\t*----- Las oraciones concatenadas -----*");
 
-Console.WriteLine($"\n{concatenada}");
+Console.WriteLine($"\n{Concatenada}");
 
+string PorcionOracion = Concatenada.Substring(0, 3);
+
+Console.WriteLine($"\n\nUna porcion de la oracion es: {PorcionOracion}");
+
+Console.WriteLine("\n\n\t\t*----- Calculadora -----*");
+
+int seguir = 1;
+
+while (seguir == 1)
+{
+    Console.WriteLine("\nOperaciones:\n\t1. Suma\n\t2. Resta\n\t3. Multiplicacion\n\t4. Division");
+
+    Console.WriteLine("\n\nIngrese el numero de la operacion que quiere realizar");
+
+    string cadena = Console.ReadLine();
+
+    OperacionElegida(cadena);
+
+    seguir = ContinuarCargandoDatos(seguir);
+}
+
+
+static float OperacionesDoblesCalculadora(int operacion, float num1, float num2)
+{
+    float resultado = 0;
+
+    switch (operacion)
+    {
+        case 1:
+            resultado = num1 + num2;
+            Console.WriteLine($"\n\t\t");
+            Console.WriteLine(@$"*----- La suma de ""{num1.ToString()}"" y de ""{num2.ToString()}"" es igual a: {resultado.ToString()} -----*");
+            break;
+        case 2:
+            resultado = num1 - num2;
+            Console.WriteLine($"\n\t\t*----- La resta de \"{num1}\" y de \"{num2}\" es igual a: {resultado} -----*\n");
+            break;
+        case 3:
+            resultado = num1 * num2;
+            Console.WriteLine($"\n\t\t*----- El producto entre \"{num1}\" y \"{num2}\" es igual a: {resultado} -----*\n");
+            break;
+        case 4:
+            if (num2 != 0)
+            {
+                resultado = num1 / num2;
+                Console.WriteLine($"\n\t\t*----- La division de \"{num1}\" en \"{num2}\" es igual a: {resultado} -----*\n");
+            }
+            else
+            {
+                Console.WriteLine("El 2do numero debe ser distinto de 0 para la division");
+            }
+            break;
+        default:
+            Console.WriteLine("Error");
+            break;
+    }
+
+    return resultado;
+}
+
+static void ControlNum2(int operacion, string operador, float num1)
+{
+    if (float.TryParse(operador, out float num2))
+    {
+        float resultado = OperacionesDoblesCalculadora(operacion, num1, num2);
+    }
+    else
+    {
+        Console.WriteLine("*----- Debe ingresar un numero como dato -----*");
+    }
+}
+
+static string ControlNum1(int operacion, string operador)
+{
+    if (float.TryParse(operador, out float num1))
+    {
+        Console.WriteLine("\nIngrese el 2do numero:");
+
+        operador = Console.ReadLine();
+        ControlNum2(operacion, operador, num1);
+    }
+    else
+    {
+        Console.WriteLine("*----- Debe ingresar un numero como dato -----*");
+    }
+
+    return operador;
+}
+
+static void OperacionElegida(string cadena)
+{
+    if (int.TryParse(cadena, out int operacion))
+    {
+        string operador;
+
+        if (operacion < 5)
+        {
+            Console.WriteLine("\n\t\t*----- Tenga en cuanta que se usa como separador decimal ',' y no '.' -----*");
+            Console.WriteLine("\nIngrese el 1er numero: ");
+
+            operador = Console.ReadLine();
+            operador = ControlNum1(operacion, operador);
+        }
+    }
+    else
+    {
+        Console.WriteLine("*----- Ingrese un dato valido -----*");
+    }
+}
+
+static int ContinuarCargandoDatos(int seguir)
+{
+    Console.WriteLine("Quiere realizar mas calculos? (Ingrese Si o No)");
+
+    string pregunta = Console.ReadLine();
+
+    if (pregunta == "no" || pregunta == "No" || pregunta == "NO" || pregunta == "nO")
+    {
+        seguir = 0;
+    }
+
+    return seguir;
+}
+
+
+Console.WriteLine("\n\nA continuacion se muestra caracter por caracter la cadena concatenada: \n");
+
+foreach (char caracter in Concatenada)
+{
+    Console.WriteLine(caracter);
+}
+
+Console.WriteLine("\n\n*----- Ingrese una palabra que desee verificar si esta en la oracion -----*");
+
+string PalabraVerificar = Console.ReadLine();
+
+string[] OracionEnPalabras = Concatenada.Split(' ');
+
+int Ocurrencias = 0;
+
+for (int i = 0; i < OracionEnPalabras.Length; i++)
+{
+    if (OracionEnPalabras[i] == PalabraVerificar)
+    {
+        Ocurrencias = Ocurrencias + 1;
+    }
+}
+
+if (Ocurrencias == 0)
+{
+    Console.WriteLine("\n\n\t*----- La palabra ingresada no esta en la oracion -----*");
+}
+else
+{
+    Console.WriteLine($"\n\n\t*----- La oracion contiene la palabra {PalabraVerificar} un total de {Ocurrencias} vez/veces -----*");
+}
+
+Console.WriteLine("\n\n\t*----- La frase en mayusculas es -----*");
+
+Console.WriteLine($"\n\n{Concatenada.ToUpper()}");
+
+Console.WriteLine("\n\n\t*----- La frase en minusculas es -----*");
+
+Console.WriteLine($"\n\n{Concatenada.ToLower()}");
+
+Console.WriteLine("\n\n\t*----- Ingrese una ecuacion simple que quiera realizar ejemplo: 345+2: ");
+
+string Ecuacion = Console.ReadLine();
+
+// string[] Numero = Ecucacion.Split('+', '-', '*', '/');
+
+string[] Numero = Ecuacion.Split(new char[] { '+', '-', '*', '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+string Operador = new string(Ecuacion.Except(Numero[0]).Except(Numero[1]).ToArray()).Trim();
+
+if (double.TryParse(Numero[0], out double num1))
+{
+    if (double.TryParse(Numero[1], out double num2))
+    {
+        double resultado;
+        switch (Operador)
+        {
+            case "+":
+                resultado = num1 + num2;
+                Console.WriteLine($"\n\t\t*----- Resultado = {resultado} -----*\n");
+                break;
+            case "-":
+                resultado = num1 - num2;
+                Console.WriteLine($"\n\t\t*----- Resultado = {resultado} -----*\n");
+                break;
+            case "*":
+                resultado = num1 * num2;
+                Console.WriteLine($"\n\t\t*----- Resultado = {resultado} -----*\n");
+                break;
+            case "/":
+                if (num2 != 0)
+                {
+                    resultado = num1 / num2;
+                    Console.WriteLine($"\n\t\t*----- Resultado = {resultado} -----*\n");
+                }
+                else
+                {
+                    Console.WriteLine("El 2do numero debe ser distinto de 0 para la division");
+                }
+                break;
+            default:
+                Console.WriteLine("Error");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("\n\n\t*----- La ecuacion ingresada no es valida -----*");
+    }
+}
+else
+{
+    Console.WriteLine("\n\n\t*----- La ecuacion ingresada no es valida -----*");
+}
